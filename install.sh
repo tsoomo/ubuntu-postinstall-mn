6ba315ba99ed42705a2a4bdea1f87949f83d1b6e
Tsolmonbaatar <tsoomo.a@gmail.com>
2025-10-04 15:42:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # Base URL of this repo (replace with your GitHub username)
-REPO_URL="https://raw.githubusercontent.com/tsoommo/ubuntu-postinstall-mn/main/scripts"
+REPO_URL="https://raw.githubusercontent.com/tsoomo/ubuntu-postinstall-mn/main/scripts"
 
 echo "[*] Running post-install scripts..."
 
